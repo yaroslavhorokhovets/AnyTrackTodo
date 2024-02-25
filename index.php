dd30282993f5c9fdd2e398e4823348918159f690
Mohit Sengar <andreievandrii71@gmail.com>
2024-02-25 22:36:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AnyTrack Test Page</title>
+</head>
+<body>
+    <form method="post" action="javascript:void(0)">
+        <input type="email" name="email" placeholder="E-mail">
+        <button type="submit">Submit Button</button>
+        <input type="button" value="Test Button" />
+    </form>
+    <div id="result"></div>
+</body>
+</html>
+<?php
+    include_once('./main.php');
+
